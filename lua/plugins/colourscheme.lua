96bb732dcacd07a1901a2c9bca198751fca9c78b
HallamSaunders <hals4072@gmail.com>
2025-06-14 15:53:07 +0100

    Added plugins like autopairs, gitsigns, which-key, etc. Changed the theme to catpuccin.

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,8 +1,34 @@
+--return {
+--  'folke/tokyonight.nvim',
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--    vim.cmd([[colorscheme tokyonight]])
+--  end
+--}
+
 return {
-  'folke/tokyonight.nvim',
-  lazy = false,
-  priority = 1000,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000, -- ensures it loads first
   config = function()
-    vim.cmd([[colorscheme tokyonight]])
-  end
+    require("catppuccin").setup({
+      flavour = "mocha",
+      integrations = {
+        indent_blankline = true,
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        telescope = true,
+        which_key = true,
+      },
+      transparent_background = true,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+      },
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end,
 }
