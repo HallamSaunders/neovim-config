7f9e96dcf357435bab66cc44525941a19181863a
HallamSaunders <hals4072@gmail.com>
2025-06-17 21:26:03 +0100

    Added GitHub Copilot plugin.

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+-- lua/plugins/copilot.lua
+return {
+  "zbirenbaum/copilot.lua",
+  config = function()
+    require("copilot").setup({
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+    })
+  end,
+}
+
