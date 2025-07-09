fb2673a8111b35bc5603bfe98647476e4804a391
HallamSaunders <hals4072@gmail.com>
2025-07-09 10:23:35 +0100

    Added conform.nvim to auto-format on save with Prettier.

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("conform").setup({
+      format_on_save = {
+        timeout_ms = 1000,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        markdown = { "prettier" },
+        yaml = { "prettier" },
+        yml = { "prettier" },
+      },
+      formatters = {
+        prettier = {
+          command = vim.fn.getcwd() .. "/frontend/node_modules/.bin/prettier",
+          args = { "--stdin-filepath", "$FILENAME" },
+          cws = vim.fn.getcwd() .. "/frontend",
+        },
+      },
+    })
+  end,
+}
