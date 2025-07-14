ee2254c8d346ba58d56a3b78c52bc8ac705f2a2b
HallamSaunders <hals4072@gmail.com>
2025-07-14 08:55:56 +0100

    Removed dotfile filtering in oil and tree.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,7 +2,11 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup({
+        filters = {
+            dotfiles = false,
+        }	
+    })
     vim.keymap.set("n", "<leader>de", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })  
   end,
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,11 @@ return {
   opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup({})
+    require("oil").setup({
+        view_options = {
+            show_hidden = true,
+        }
+    })
     -- Example keymap to open Oil in the current directory
     vim.keymap.set("n", "-", "<CMD>oil<CR>", { desc = "Open parent directory" })
   end
