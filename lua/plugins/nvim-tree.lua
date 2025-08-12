1dcd33e3c23b74f9b5fcba2150c648564075e222
HallamSaunders <hals4072@gmail.com>
2025-08-12 09:15:33 +0100

    Added auto-resizing for nvim tree, and removed sidescrolling.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,10 +3,30 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("nvim-tree").setup({
-        filters = {
-            dotfiles = false,
-        }	
+      filters = {
+        dotfiles = false,
+      },
+      view = {
+        width = {
+          min = 30,
+          max = -1,
+          padding = 1,
+        },
+        preserve_window_proportions = true,
+      }
     })
-    vim.keymap.set("n", "<leader>de", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })  
+
+    -- Keymaps
+    vim.keymap.set("n", "<leader>de", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
+    -- Disable side scrolling
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "NvimTree",
+      callback = function()
+        vim.opt_local.wrap = false
+        vim.opt_local.sidescroll = 0
+        vim.opt_local.sidescrolloff = 0
+      end,
+    })
   end,
 }
