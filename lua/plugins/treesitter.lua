06b2ab52337f2b323855f2c14f2f6f553dd7a26d
HallamSaunders <hals4072@gmail.com>
2026-02-03 18:24:08 +0000

    Updated.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,15 +2,19 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "python", "javascript", "typescript", "lua", "html", "css", "json", "bash", "yaml", "markdown", "markdown_inline", "go", "rust" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-      })
-    end,
-  }
+    opts = {
+      ensure_installed = {
+        "python", "javascript", "typescript", "lua",
+        "html", "css", "json", "bash", "yaml",
+        "markdown", "markdown_inline",
+        "go", "rust",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    },
+  },
 }
+
