38e965278787e0c6384d27b7b364f1526c4bf281
hallam <hals4072@gmail.com>
2026-03-03 16:33:13 +0000

    Update.

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -7,6 +7,7 @@ return {
     vim.g.vimtex_compiler_latexmk = {
       out_dir = "build",
       options = {
+        "-lualatex",
         "-shell-escape",
         "-synctex=1",
         "-verbose",
